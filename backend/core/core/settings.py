f793b5a3c4e6f57ac0e716261fc52af11a022e27
Sagar <sagarshuklaloved@gmail.com>
2026-08-26 21:10:21 +0530

    add alllowed hosts

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0%+5!gn)vx9gex89inoa&uv3j4gsa+*zzximz2tmad7(^y-t8x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

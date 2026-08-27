249db5fb044351c5ebe4ed7ffa625ebf2e4b31a7
Sagar <sagarshuklaloved@gmail.com>
2026-08-27 12:53:48 +0530

    Connect Django to PostgreSQL

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -72,11 +72,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
